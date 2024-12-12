600d4cbc4b7f6d9d5360cda87aa40705f502c9b7
PetrMachacka <91247837+PetrMachacka@users.noreply.github.com>
2024-12-12 19:53:05 +0000

    ScreenShot

--- MaturitniProjekt/Assets/Scripts/UI/PauseMenu.cs
+++ MaturitniProjekt/Assets/Scripts/UI/PauseMenu.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System;
+using System.IO;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject uploadMenu;
     public static bool isPaused = false;
+    public static bool pictureMode = false;
     private void Start() {
         isPaused = false;
         uploadMenu.SetActive(false);
@@ -16,7 +15,9 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
     }
     private void OnESCAPE(){
-        Debug.Log("ESCAPE");
+        TogglePauseMenu();
+    }
+    private void TogglePauseMenu(){
         if(!pauseMenu.activeSelf){
             pauseMenu.SetActive(true);
             Time.timeScale = 0f;
@@ -29,7 +30,25 @@ public class PauseMenu : MonoBehaviour
         }
     }
     private void OnPhoto(){
-        Debug.Log("Photo");
+        if(pictureMode)
+        {
+            TakeScreenshot();
+            pictureMode = false;
+        }
+    }
+    private void TakeScreenshot()
+    {
+        string screenshotFolder = Path.Combine(Application.persistentDataPath, $"Levels/{PlayerPrefs.GetString("SelectedLevel")}");
+        if (!Directory.Exists(screenshotFolder))
+        {
+            Directory.CreateDirectory(screenshotFolder);
+        }
+
+        string screenshotName = $"preview.png";
+        string screenshotPath = Path.Combine(screenshotFolder, screenshotName);
+
+        ScreenCapture.CaptureScreenshot(screenshotPath);
+        Debug.Log($"Screenshot saved to: {screenshotPath}");
     }
     public void LevelsButton()
     {
@@ -55,5 +74,10 @@ public class PauseMenu : MonoBehaviour
     public async void SteamUpload()
     {
         await SteamManager.UploadLevelToSteamWorkshopAsync(PlayerPrefs.GetString("SelectedLevel"));
+    }
+    public void PictureButton()
+    {
+        pictureMode = true;
+        TogglePauseMenu();
     }
 }
