dd07245d4ae5d0db1e8829982fb1fdfc73e4432e
PetrMachacka <91247837+PetrMachacka@users.noreply.github.com>
2024-12-17 16:32:23 +0000

    Inventory

--- MaturitniProjekt/Assets/Scripts/EditorScripts/Inventory.cs
+++ MaturitniProjekt/Assets/Scripts/EditorScripts/Inventory.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour, EditorInputSystem.IEditorActions
 {
@@ -14,6 +15,10 @@ public class Inventory : MonoBehaviour, EditorInputSystem.IEditorActions
         inputSystem = new EditorInputSystem();
         inputSystem.Editor.SetCallbacks(this);
     }
+    private void Start()
+    {
+        PopulateInventory();
+    }
 
     private void OnEnable()
     {
@@ -23,6 +28,34 @@ public class Inventory : MonoBehaviour, EditorInputSystem.IEditorActions
     private void OnDisable()
     {
         inputSystem.Editor.Disable();
+    }
+
+    private void PopulateInventory()
+    {
+        List<GameObject> inventorySlots = new List<GameObject>();
+        foreach (Transform child in inventoryPanel.transform)
+        {
+            inventorySlots.Add(child.gameObject);
+            Debug.Log(child.name);
+        }
+
+        GameObject[] prefabs = Resources.LoadAll<GameObject>("Prefabs/Build");
+        foreach (GameObject prefab in prefabs)
+        {
+            Debug.Log(prefab.name);
+        }
+        for (int i = 0; i < inventorySlots.Count && i < prefabs.Length; i++)
+        {
+            GameObject slot = inventorySlots[i];
+            Debug.Log(slot.name);
+            GameObject prefab = prefabs[i];
+            if (slot.TryGetComponent<Image>(out Image slotImage))
+            if (slotImage != null)
+            {
+                Sprite prefabSprite = prefab.GetComponent<SpriteRenderer>().sprite;
+                slotImage.sprite = prefabSprite;
+            }
+        }
     }
 
     public void OnNumberKeys(InputAction.CallbackContext context)
